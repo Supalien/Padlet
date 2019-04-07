b52be6ec228d04ac6fba0249964c13c07b29a677
Doron <dorons900@gmail.com>
2019-04-07 18:46:09 +0300

    Changed get_status to print_status

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@ from .extract_cards import load, Padlet
 from .get_csv import get
 import os
 
-def get_status():
+def print_status():
     p = load(get(path = "C:/temp/Padlet.csv"))
     print(p.status())
     os.remove("C:/temp/Padlet.csv")
